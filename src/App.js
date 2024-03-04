541ab3a6eaa8d894085088353f1941870aac22e3
RavannaW <jpxemma@gmail.com>
2024-03-03 21:47:38 -0500

    add notification

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import Landing_Page from './Components/LandingPage/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import Notification from './Components/Notification/Notification';
 
 function App() {
 
   return (
     <div className="App">
         <BrowserRouter>
+          <Notification>
           <Navbar/>
           <Routes>
             <Route path="/" element={<Landing_Page />}/>
             <Route path="/signup" element={<Sign_Up/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="<component_route>" element={<component_name/>}/>
           </Routes>
+          </Notification>
         </BrowserRouter>
-       
     </div>
   );
 }
