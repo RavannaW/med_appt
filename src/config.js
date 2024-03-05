89b8799f2642994f2ab2111fb2a9ed7bedb5cf3a
RavannaW <jpxemma@gmail.com>
2024-03-05 14:45:13 -0500

    build and deploy website

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = window.location.hostname === "localhost" ? "https://jpxemma-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "add your theia server side url";
-export const API_URL = window.location.hostname === "localhost" ? "https://jpxemma-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://jpxemma-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
-// export const API_URL = window.location.origin;
+// export const API_URL = window.location.hostname === "localhost" ? "https://jpxemma-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://jpxemma-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
+export const API_URL = window.location.origin;
 console.log(
     "API_URL :",
     API_URL
